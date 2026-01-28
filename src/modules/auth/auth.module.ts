cb4b082c0cad7c6e3ae5af3d69b7a9289a06ada7
josephus <josephusmupanda48@gmail.com>
2026-01-28 10:40:11 +0200

    add auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,47 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { RefreshTokenStrategy } from './strategies/refresh-token.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+import { User } from '../users/entities/user.entity';
+import { Donor } from '../donations/entities/donor.entity';
+import { Staff } from '../users/entities/staff.entity';
+import { Beneficiary } from '../beneficiaries/entities/beneficiary.entity';
+import { Helpers } from '../../shared/utils/helpers';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, Donor, Staff, Beneficiary]),
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('config.jwt.secret'),
+        signOptions: {
+          expiresIn: configService.get('config.jwt.expiresIn'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    UsersService,
+    JwtStrategy,
+    RefreshTokenStrategy,
+    LocalStrategy,
+    Helpers,
+  ],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
