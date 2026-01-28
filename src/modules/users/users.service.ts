fa156a4dfaf5abbde7d4860cd481496584c86a85
josephus <josephusmupanda48@gmail.com>
2026-01-28 10:41:54 +0200

    add user service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,101 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { BaseService } from '../../shared/services/base.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PaginationParams } from '../../shared/interfaces/pagination.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService extends BaseService<User> {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {
+    super(usersRepository);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async findByPhone(phone: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { phone } });
+  }
+
+  async findByEmailOrPhone(identifier: string): Promise<User | null> {
+    return this.usersRepository.findOne({
+      where: [{ email: identifier }, { phone: identifier }],
+    });
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return this.findOne(id);
+  }
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(user);
+  }
+
+  async updateUser(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.findById(id);
+    
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    Object.assign(user, updateUserDto);
+    return this.usersRepository.save(user);
+  }
+
+  async deactivateUser(id: string): Promise<User> {
+    const user = await this.findById(id);
+    
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    user.isActive = false;
+    return this.usersRepository.save(user);
+  }
+
+  async activateUser(id: string): Promise<User> {
+    const user = await this.findById(id);
+    
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    user.isActive = true;
+    return this.usersRepository.save(user);
+  }
+
+  async updateLastLogin(id: string): Promise<void> {
+    await this.usersRepository.update(id, {
+      lastLoginAt: new Date(),
+    });
+  }
+
+  async searchUsers(query: string, paginationParams: PaginationParams) {
+    const where = query
+      ? [
+          { email: query },
+          { phone: query },
+          { fullName: query },
+        ]
+      : undefined;
+
+    return this.paginate(paginationParams, where);
+  }
+
+  async getUsersByType(userType: string, paginationParams: PaginationParams) {
+    return this.paginate(paginationParams, { userType });
+  }
+
+  async countUsersByType(userType?: string): Promise<number> {
+    const where = userType ? { userType } : undefined;
+    return this.count(where);
+  }
+}
